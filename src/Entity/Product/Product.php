ae6bd8e2b2db4d8edabcea30f941e2f2dc5139af
em411 <lime4113@gmail.com>
2021-06-28 17:24:13 +0200

    Change mappings type to xml

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
-use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product")
- */
 class Product extends BaseProduct
 {
-    /** @ORM\Column(type="string", nullable=true) */
     private $color;
 
     public function getColor(): ?string
