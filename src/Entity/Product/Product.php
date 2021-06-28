a3dac43d04965cc628873b43889c493a7f13b58e
em411 <lime4113@gmail.com>
2021-06-28 15:01:52 +0200

    Update Product model with Color field

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -14,6 +14,19 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
  */
 class Product extends BaseProduct
 {
+    /** @ORM\Column(type="string", nullable=true) */
+    private $color;
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor($color): void
+    {
+        $this->color = $color;
+    }
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
